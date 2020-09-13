5d66e8b3506db70f871cd17d61a709cff19d336d
Mohamed AmineBaccar <Mohamed.AmineBaccar@lombardrisk.com>
2020-09-13 14:35:02 +0100

    first commit

---
+++ src/main/java/MainClazz.java
@@ -0,0 +1,26 @@
+import java.util.logging.Logger;
+
+public class MainClazz {
+
+    public Integer multiply(Integer a, Integer b){
+        return a * b;
+    }
+
+    public Integer add(Integer a, Integer b){
+        return a +b;
+    }
+
+    public Integer substract(Integer a, Integer b){
+        return a - b ;
+    }
+
+    public Integer divide(Integer a, Integer b){
+        return a / b ;
+    }
+
+    public static void main (String[] args){
+        Logger logger = Logger.getLogger("");
+        logger.info("Hello World!");
+//        logger.info(multiply(10,5)+"");
+    }
+}
---
+++ src/test/java/MainClazzTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class MainClazzTest {
+    private MainClazz mainClazz = new MainClazz();
+
+    @Test
+    void multiply() {
+        int result = mainClazz.multiply(10,45);
+        Assert.assertEquals(450, result);
+    }
+
+    @Test
+    void divide() {
+        int result = mainClazz.divide(100,10);
+        Assert.assertEquals(10, result);
+    }
+
+    @Test
+    void add() {
+        int result = mainClazz.add(100,1);
+        Assert.assertEquals(101, result);
+    }
+
+    @Test
+    void substract() {
+        int result = mainClazz.substract(100,1);
+        Assert.assertEquals(99, result);
+    }
+}
